967d8771e79900552fa90b1182d956305b85c549
gabrielruz43 <gabrielruz43@gmail.com>
2022-06-18 21:38:04 +0300

    Added controller context example.

---
+++ ControllerContext/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ControllerContext.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task Index()
+        {
+            Response.ContentType = "text/html; charset=utf-8";
+            StringBuilder sb = new StringBuilder("<h2>Request headers</h2><table>");
+
+            foreach (var header in Request.Headers)
+            {
+                sb.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+            }
+
+            sb.Append("</table>");
+            await Response.WriteAsync(sb.ToString());
+        }
+    }
+}
